09aa928f90aa9e6e83ba60801bd929673bf7287b
Navendu Singh <54157440+NavenduSingh07@users.noreply.github.com>
2022-12-23 12:09:53 +0530

    Update String.java

--- String.java
+++ String.java
@@ -46,3 +46,28 @@ class DoNotTerminate {
   System.setSecurityManager(securityManager);
  }
 }
+
+
+// You are given an integer , you have to convert it into a string.
+
+// Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
+
+//  can range between  to  inclusive.
+
+// Sample Input 0
+
+// 100
+// Sample Output 0
+
+// Good jobYou are given an integer , you have to convert it into a string.
+
+// Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
+
+//  n can range between 100 to 100 inclusive.
+
+// Sample Input 0
+
+// 100
+// Sample Output 0
+
+// Good job
